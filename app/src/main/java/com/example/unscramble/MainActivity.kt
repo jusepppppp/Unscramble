87ac0da06bdf8b6b160eac449272920144f828eb
Admin <bitengmarkjoseph0@gmail.com>
2026-09-20 13:37:40 +0800

    fifth commit phase 5

--- app/src/main/java/com/example/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -50,6 +50,12 @@ fun GameScreen() {
 
     val correctAnswer = words[currentWordIndex]
 
+    var scrambledWord by remember {
+        mutableStateOf(
+            String(words[0].toCharArray().apply { shuffle() })
+        )
+    }
+
     var score by remember {
         mutableStateOf(0)
     }
@@ -65,7 +71,7 @@ fun GameScreen() {
         )
 
         Text(
-            text = correctAnswer,
+            text = scrambledWord,
             fontSize = 40.sp
         )
 
@@ -91,6 +97,8 @@ fun GameScreen() {
                     if (currentWordIndex < words.size - 1) {
                         currentWordIndex++
                         userAnswer = ""
+
+                        scrambledWord = String(words[currentWordIndex].toCharArray().apply { shuffle() })
                     }
                 }
             }
